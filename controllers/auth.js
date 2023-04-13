53870e0d08d381e6d17d2b98560bb048eb8e523a
InspUsman <fawad@inspedium.com>
2023-04-13 14:11:22 +0500

    email section completed!

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,32 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendGridTransport = require("nodemailer-sendgrid-transport");
+// My solution
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const User = require("../models/user");
+
+// setup telling nodemailer how the emails will be delivered
+// const transporter = nodemailer.createTransport(
+//   sendGridTransport({
+//     auth: {
+//       api_key:
+//         "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",
+//     },
+//   })
+// );
+
+const transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "b3463a300b832c",
+    pass: "2b9cf116e19102",
+  },
+});
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -78,7 +105,27 @@ exports.postSignup = async (req, res, next) => {
     password: hashedPassword,
     cart: { items: [] },
   });
-  await user.save();
+  // await user.save();
+
+  // returns a promise so we can use await, but not necessary here
+  transporter.sendMail({
+    to: email,
+    from: "support@nodeshop.com",
+    subject: "Sign Up Succeeded!",
+    html: "<h1>You successfully signed up!</h1>",
+  });
+
+  // try {
+  //   await sgMail.send({
+  //     to: email,
+  //     from: "usman1706@hotmail.com",
+  //     subject: "Sign Up Succeeded!",
+  //     text: "You successfully signed up!",
+  //     html: "<h1>You successfully signed up!</h1>",
+  //   });
+  // } catch (err) {
+  //   console.log(err);
+  // }
 
   res.redirect("/login");
 };
